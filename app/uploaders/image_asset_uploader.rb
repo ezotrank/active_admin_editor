7e5d918da5fb97452cfdf80a38139820f5be5dcc
Maxim Kremenev <ezotrank@gmail.com>
2012-10-30 23:03:53 +0300

    move carrierwave store_dir from uploads to system/uploads

--- app/uploaders/image_asset_uploader.rb
+++ app/uploaders/image_asset_uploader.rb
@@ -35,7 +35,7 @@ class ImageAssetUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :three_quarters do
